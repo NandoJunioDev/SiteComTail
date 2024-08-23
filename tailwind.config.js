8a60d6a20929da1eefd5c941651dca36b684ab30
NandoJunioDev <Frnndmaz_cf@outlook.com>
2024-08-23 17:06:25 -0300

    work-23/08

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       brightness:{
         25:'.25',
-      }
+      },
+      backgroundImage:{
+        'backgroud-main-section': "url(../imagem/Material/technology-791029_1920.webp)",
+      },
     },
   },
   plugins: [],
