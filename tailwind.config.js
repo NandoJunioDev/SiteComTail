cb1b1e8bb024f49dcbe41fecb3723948abaaff6f
NandoJunioDev <frnndmaz_cf@outlook.com>
2024-09-01 22:02:45 -0300

    update 01/09/2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,24 +16,29 @@ module.exports = {
             'blue-2':'#1755A6',
             'blue-3':'#6A9AD9',
             'blue-4':'#91B7D9',
-            'marfin':'#D9985F'
+            'marfin':'#D9985F',
+            'preto':'#212426',
+            'cinza':'#D9D9D9',
+            'verde':'#E2F266',
+            'verde-2':'#9CA653',
+            'amarelo':'#F2EA77',
       },
-      textShadow: {
-        'sm':'0 1px 2px var(--tw-shadow-color)',
-        'DEFAULT':'0 2px 4px var(--tw-shadow-color)',
-        'lg':'0 8px 16px var(--tw-shadow-color)',
-      
-    },
+    
       fontFamily:{
         'Russo-One-Regular':['Russo-One'],
         'Raleway-Reular':['Raleway-Regular']
       },
       screens:{
-        'celular':'310px' 
+        'celular':{'max': '420px'}
 
       },
       gridTemplateRows:{
         'row-3-fit':'repeat(auto-fill, minmax(100px,1fr))'
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
       },
   },
    plugins: [
